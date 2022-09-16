c26f9d5904bb13eb9a8830f224bbedd8f36094ca
lanomade <ganne.hannah@gmail.com>
2022-09-17 01:23:40 +0200

    add about page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Gallery from './pages/Gallery'
 import Error from './pages/Error'
 import Detail from './pages/Detail'
+import About from './pages/About'
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} >
                     <Route path="/" element={<Gallery />} />
-                    <Route path="/:id" element={<Detail />} />
+                    <Route path=":id" element={<Detail />} />
+                    <Route path="about" element={<About />} />
                     <Route path="*" element={<Error />} />
                 </Route>
             </Routes>
